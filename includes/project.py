aa300d883adf41ed38feb9cd787d216c65328c1c
Derek Enos <derek.enos@gmail.com>
2021-01-11 14:11:26 -0500

    Added project data to context object and generated missing project thumbs.

--- includes/project.py
+++ includes/project.py
@@ -10,14 +10,15 @@ from macros import picture
 
 Head = NotDefined
 
-Body = lambda context, name, desc, tags, img_base_fn, img_alt: (
-    H1(name),
-    H2(desc),
-    H3(' '.join(f'#{tag}' for tag in tags)),
-    *picture.Body(
-        context,
-        srcsets=(context.static(f'{img_base_fn}.webp'),),
-        src=context.static(f'{img_base_fn}.png'),
-        alt=img_alt
+def Body(context, name, description, tags, thumb_img_base_fn, thumb_img_alt):
+    return (
+        H1(name),
+        H2(description),
+        H3(' '.join(f'#{tag}' for tag in tags)),
+        *picture.Body(
+            context,
+            srcsets=(context.static(f'{thumb_img_base_fn}.webp'),),
+            src=context.static(f'{thumb_img_base_fn}.png'),
+            alt=thumb_img_alt
+        )
     )
-)
--- pages/index.py
+++ pages/index.py
@@ -12,15 +12,10 @@ Head = lambda context: (
 
 Body = lambda context: (
     Div(
-        _class='item',
-        children=project.Body(
-            context,
-            name='Cardboard Boxcade',
-            desc='Custom physical controls for browser-based HTML/Javascript '\
-            'applications.',
-            tags=('esp32', 'micropython', 'iot', 'videogame'),
-            img_base_fn='cardboard_boxcade_thumb',
-            img_alt='photo of a cardboard box with a screen and switches'
-        )
+        _class="content",
+        children=[
+            Div(_class='item', children=project.Body(context, **prj))
+            for prj in context.projects
+        ]
     ),
 )
