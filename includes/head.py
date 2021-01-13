6f657f73b7221fd0fcb21261ea7407c59f2ec5e8
Derek Enos <derek.enos@gmail.com>
2021-01-13 15:57:45 -0500

    Renamed includes/auto_reload to includes/live_dev because it now also does build error reporting.

--- includes/head.py
+++ includes/head.py
@@ -3,10 +3,10 @@ from itertools import chain
 
 from lib.htmlephant_extensions import Link
 from . import google_analytics
-from . import auto_reload
+from . import live_dev
 
 Head = lambda context: chain(
-    auto_reload.Head(context) if not context.production else (),
+    live_dev.Head(context) if not context.production else (),
     google_analytics.Head(context) if context.production else (),
     (Link(rel='stylesheet', href=context.static('shared.css')),)
 )
