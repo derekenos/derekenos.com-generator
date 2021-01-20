6ad085faa37cc0990d733d225f96d298d83ea9b8
Derek Enos <derek.enos@gmail.com>
2021-01-20 12:14:12 -0500

    Server now auto-resolves to directory index.html

--- lib/server.py
+++ lib/server.py
@@ -6,6 +6,7 @@ import os
 from .femtoweb.femtoweb.filesystem_endpoints import _fs_GET
 from .femtoweb.femtoweb.server import (
     _200,
+    _303,
     GET,
     POST,
     route,
@@ -46,14 +47,24 @@ def serve(site_dir, host, port):
     @route('.*', methods=(GET,))
     async def get(request):
         req_path = request.path.lstrip('/')
+        fs_path = os.path.join(site_dir, req_path)
         if req_path == '':
             # Return index.html as root.
             req_path = 'index.html'
-        elif (not os.path.exists(os.path.join(site_dir, req_path))
-              and not req_path.endswith('.html')):
+        elif os.path.isdir(fs_path):
+            if not req_path.endswith('/'):
+                # If req_path references as directory but no trailing slash
+                # was specified, redirect to the same path with trailing slash.
+                return _303(location=f'/{req_path}/')
+            if os.path.exists(os.path.join(fs_path, 'index.html')):
+                # If req_path is a directory that contains an index.html,
+                # return that file.
+                req_path = f'{req_path.rstrip("/")}/index.html'
+        elif (not os.path.exists(fs_path) and not req_path.endswith('.html')):
             # To mimic how common webservers work, resolve a path to an .html
             # file without having to specify it in the path.
             req_path += '.html'
+
         return _fs_GET(site_dir, req_path)
 
     # Start the server.
