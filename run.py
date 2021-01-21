4d97c9fc986c49964bc46bfcc3e345ad68ce120e
Derek Enos <derek.enos@gmail.com>
2021-01-21 09:17:50 -0500

    Moved Context into new lib.context module.

--- run.py
+++ run.py
@@ -3,13 +3,10 @@ import argparse
 import json
 import os
 from itertools import chain
-from datetime import datetime
 from glob import glob
 
-from lib import (
-    copy_if_newer,
-    large_static_store,
-)
+from lib import copy_if_newer
+from lib.context import Context
 from lib.htmlephant import Document
 from lib.server import serve
 
@@ -18,103 +15,12 @@ import includes.body
 import includes.footer
 
 ###############################################################################
-# Context Class
-###############################################################################
-
-class Context:
-    PAGES_DIR = 'pages'
-    STATIC_DIR = 'static'
-    SITE_DIR = 'site'
-    SITE_RELATIVE_STATIC_DIR = 'static'
-    SITE_STATIC_DIR = f'{SITE_DIR}/{SITE_RELATIVE_STATIC_DIR}'
-    STATIC_LARGE_FILE_THRESHOLD_MB = 1
-    SITE_RELATIVE_LARGE_STATIC_DIR = f'static/large'
-    SITE_LARGE_STATIC_DIR = f'{SITE_DIR}/{SITE_RELATIVE_LARGE_STATIC_DIR}'
-
-    def __init__(self, production=False, **kwargs):
-        self.production = production
-        # Generate a list of page names in self.PAGES_DIR.
-        self.page_names = [
-            mod_name.rsplit('.', 1)[0]
-            for mod_name in os.listdir(self.PAGES_DIR)
-            if mod_name.endswith('.py')
-        ]
-        # Initialize runtime attributes.
-        self.current_page = None
-        self.generator_item = None
-        # Add any custom attributes.
-        for k, v in kwargs.items():
-            setattr(self, k, v)
-
-        # Maybe instantiate a large static store as the attribute 'lss'.
-        if self.large_static_store is not None:
-            assert not hasattr(self, 'lss')
-            setattr(
-                self, 'lss', large_static_store.get(self.large_static_store)
-            )
-
-    def static_last_modified_iso8601(self, filename):
-        return datetime.fromtimestamp(
-            os.stat(os.path.join(self.STATIC_DIR, filename)).st_mtime
-        ).isoformat()
-
-    def static(self, filename):
-        """Format filename as a static asset path, assert that the file exists,
-        and return the path.
-        """
-        fs_path = f'{self.STATIC_DIR}/{filename}'
-        # Check that file exists either locally or in the lss.
-        if not os.path.isfile(fs_path):
-            # If the file exist in the lss, return that URL.
-            if not hasattr(self, 'lss') or not self.lss.exists(filename):
-                raise AssertionError(f'Path is not a regular file: {fs_path}')
-            # File exists in lss.
-            path = f'{self.large_static_store["endpoint"]}/{filename}'
-        elif not self.is_large_static(filename):
-            # It's a small, local file.
-            path = f'{self.SITE_RELATIVE_STATIC_DIR}/{filename}'
-        else:
-            # It's a large file.
-            if self.production:
-                path = f'{self.large_static_store["endpoint"]}/{filename}'
-            else:
-                # Warn if the file exists both locally and in the lss manifest.
-                if hasattr(self, 'lss') and self.lss.exists(filename):
-                    print(f'Large, local file "{filename}" exists in the LSS.')
-                path = os.path.join(
-                    self.SITE_RELATIVE_LARGE_STATIC_DIR,
-                    filename
-                )
-        return path
-
-    def is_large_static(self, filename):
-        path = f'{self.STATIC_DIR}/{filename}'
-        if not os.path.isfile(path):
-            raise AssertionError(f'Path is not a regular file: {path}')
-        return os.stat(path).st_size / 1024 / 1024 \
-            >= self.STATIC_LARGE_FILE_THRESHOLD_MB
-
-    def url(self, path):
-        """Return path as an absolute URL.
-        """
-        return f'{self.base_url if self.production else ""}/{path.lstrip("/")}'
-
-    def static_url(self, filename):
-        """Return an absolute URL for a static asset filename.
-        """
-        return self.url(self.static(filename))
-
-    def open(self, path):
-        """Return a writable UTF-8 file handle for a self.SITE_DIR sub-path.
-        """
-        path = os.path.join(self.SITE_DIR, path.lstrip('/'))
-        return open(path, 'w', encoding='utf-8')
-
-###############################################################################
-# Run Function
+# Site file writer helpers
 ###############################################################################
 
 def write_page(context, filename, page_mod):
+    """Write a single HTML site page.
+    """
     # Open the HTML output file.
     with context.open(filename) as fh:
         # Combine global includes with the module Head and Body to create
@@ -130,6 +36,23 @@ def write_page(context, filename, page_mod):
         # Write the document to the file.
         for c in doc:
             fh.write(c)
+
+def write_sitemap(context, filenames):
+    with context.open(context.SITEMAP_FILENAME) as fh:
+        for filename in filenames:
+            fh.write(f'{context.base_url}/{filename}\n')
+
+def write_robots(context):
+    with context.open('robots.txt') as fh:
+        fh.write(
+f"""User-agent: *
+Allow: /
+Sitemap: {context.base_url}/{context.SITEMAP_FILENAME}
+""")
+
+###############################################################################
+# Static file copy helper
+###############################################################################
 
 def copy_static(context):
     """Copy files from the local static directory to the site
@@ -151,12 +74,12 @@ def copy_static(context):
         filename = fs_path[static_dir_len + 1:]
         # Check whether the file exceeds the large object threshold.
         if context.is_large_static(filename):
-            # This is a large file - create a symlink in the destination directory
-            # instead of actually copying it.
+            # This is a large file - create a symlink in the destination
+            # directory instead of actually copying it.
             dest = os.path.join(context.SITE_LARGE_STATIC_DIR, filename)
             if not os.path.lexists(dest):
-                # Use the absolute file system path as the symlink src instead of
-                # trying to figure out how many parent dirs to references.
+                # Use the absolute file system path as the symlink src instead
+                # of trying to figure out how many parent dirs to references.
                 os.symlink(
                     os.path.join(os.path.dirname(__file__), fs_path),
                     dest
@@ -164,6 +87,10 @@ def copy_static(context):
         else:
             dest = os.path.join(context.SITE_STATIC_DIR, filename)
             copy_if_newer(fs_path, dest)
+
+###############################################################################
+# Run Function
+###############################################################################
 
 def run(context):
     # Copy static files to output, creating dirs as necessary.
@@ -198,19 +125,9 @@ def run(context):
                 write_page(context, filename, page_mod)
                 filenames.append(filename)
 
-    # Write sitemap.txt
-    with context.open('sitemap.txt') as fh:
-        for filename in filenames:
-            fh.write(f'{context.base_url}/{filename}\n')
-
-    # Write robots.txt
-    with context.open('robots.txt') as fh:
-        fh.write(
-f"""User-agent: *
-Allow: /
-Sitemap: {context.base_url}/sitemap.txt
-""")
-
+    # Write the sitemap and robots files.
+    write_sitemap(context, filenames)
+    write_robots(context)
 
 ###############################################################################
 # CLI
@@ -226,14 +143,13 @@ if __name__ == '__main__':
     parser.add_argument('--sync-large-static', action='store_true')
     args = parser.parse_args()
 
-    # Set the default Context kwargs.
-    ctx_kwargs = { 'production': not args.development }
-
-    # Attempt to read the context file.
+    # Instantiate the Context object.
     if not os.path.exists(args.context_file):
         parser.error(f'Context file not found: {args.context_file}')
-    ctx_kwargs.update(json.load(open(args.context_file, 'rb')))
-    context = Context(**ctx_kwargs)
+    context = Context(
+        production=not args.development,
+        **json.load(open(args.context_file, 'rb'))
+    )
 
     # Generate the site files.
     run(context)
