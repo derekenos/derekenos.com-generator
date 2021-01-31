ade8048dcf11704afd503b37a06689377bb3ab75
Derek Enos <derek.enos@gmail.com>
2021-01-30 19:27:45 -0500

    Added a bunch of meta tags to tag pages.

--- pages/tag_generator.py
+++ pages/tag_generator.py
@@ -35,9 +35,36 @@ CONTEXT_ITEMS_GETTER = lambda context: [
 ]
 
 def Head(context):
-    return (
-        Title(f'{context.name} | #{context.generator_item["name"]}'),
+    """Return a list of Meta elements comprising tag metadata.
+    See:
+      https://developer.mozilla.org/en-US/docs/Web/HTML/Element/meta/name
+      https://ogp.me/#metadata
+    """
+    tag = context.generator_item
+    els = []
+    # Author.
+    els.append(StdMeta('author', context.name))
+    # Description.
+    description = f'A collection of projects associated with the tag #{tag["name"]}.'
+    els.extend((
+        StdMeta('description', description),
+        OGMeta('description', description)
+    ))
+    # Keywords
+    els.append(
+        StdMeta('keywords', ', '.join((tag['name'], 'tag', 'projects')))
     )
+    # Title
+    title = f'{context.name} | #{tag["name"]}'
+    els.extend((
+        OGMeta('title', title),
+        Title(title)
+    ))
+    # Type
+    els.append(OGMeta('type', 'website'))
+    # URL
+    els.append(OGMeta('url', context.url(tag['slug'])))
+    return els
 
 def Nav(context):
     current_tag = context.generator_item
@@ -51,20 +78,23 @@ def Nav(context):
     ]
     return subnav.Body(context, name_url_pairs)
 
-Body = lambda context: (
-    Div(
-        _class='content tag',
-        children=(
-            H1(desc:=f'Projects Tagged #{(tag:=context.generator_item["name"])}'),
-            *collection.Body(
-                context,
-                name=desc,
-                items=[
-                    project_card.Body(context, **project)
+def Body(context):
+    tag_name = context.generator_item["name"]
+    description = f'Projects Tagged #{tag_name}'
+    return (
+        Div(
+            _class='content tag',
+            children=(
+                H1(description),
+                *collection.Body(
+                    context,
+                    name=description,
+                    items=[
+                        project_card.Body(context, **project)
                     for project in context.projects
-                    if tag in project['tags']
-                ]
+                    if tag_name in project['tags']
+                    ]
+                )
             )
-        )
-    ),
-)
+        ),
+    )
